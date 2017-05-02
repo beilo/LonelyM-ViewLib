cf00e22a4457d3c49454ba25a31babdf338e808d
leipeng <leipeng@czcg.com.cn>
2017-05-02 10:11:53 +0800

    修复dialogupdate在某些手机上有title空白

--- ViewComponent/viewlib/src/main/java/com/czcg/viewlib/widget/DialogUpdateWidget.java
+++ ViewComponent/viewlib/src/main/java/com/czcg/viewlib/widget/DialogUpdateWidget.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.RelativeLayout;
 
 import com.czcg.viewlib.R;
@@ -30,14 +31,16 @@ public class DialogUpdateWidget {
         View view = LayoutInflater.from(context).inflate(R.layout.dialog_update, null);// 得到加载view
         progressBar = (NumberProgressBar) view.findViewById(R.id.progress_bar);
         dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);  //http://blog.csdn.net/u011747761/article/details/47419419
         dialog.setCancelable(isCancel);
         dialog.setCanceledOnTouchOutside(isCancel);
         dialog.show();
-        dialog.setContentView(view, new RelativeLayout.LayoutParams(
+        dialog.addContentView(view, new RelativeLayout.LayoutParams(
                 DensityConversionUtil.dpToPx(context, 250),
                 DensityConversionUtil.dpToPx(context, 100)));
         return dialog;
     }
+
 
     public void setProgress(int position) {
         progressBar.setProgress(position);
